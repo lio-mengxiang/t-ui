68915355b61f43d50d14d6e11553ea6de2b0c5a8
liomeng <liomeng@vastaitech.com>
2025-12-27 21:20:43 +0800

    fix: fix homeurl

--- apps/react-pc-website/app/page.tsx
+++ apps/react-pc-website/app/page.tsx
@@ -2,10 +2,10 @@ export default function RootPage() {
   return (
     <html lang="zh">
       <head>
-        <meta httpEquiv="refresh" content="0; url=/zh" />
+        <meta httpEquiv="refresh" content={`0; url=${process.env.homeUrl}`} />
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.location.href = '/zh'`,
+            __html: `window.location.href = '${process.env.homeUrl}'`,
           }}
         />
       </head>
--- apps/react-pc-website/next.config.ts
+++ apps/react-pc-website/next.config.ts
@@ -29,9 +29,7 @@ const nextConfig: NextConfig = {
   basePath: getBasePath(),
   env: {
     isGithubPage: getBasePath(),
-    githubLink: 'https://github.com/lio-mengxiang/t-ui',
-    defaultTheme: 'light',
-    defaultLang: 'zh',
+    homeUrl: getHomePath(),
   },
   images: { unoptimized: true },
   /* config options here */
