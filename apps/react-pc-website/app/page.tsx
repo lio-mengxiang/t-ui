428db67fa68088cad789adc7f668d55e057cdc3d
liomeng <liomeng@vastaitech.com>
2026-02-12 23:23:29 +0800

    feat: add robots.txt

--- apps/react-pc-website/app/page.tsx
+++ apps/react-pc-website/app/page.tsx
@@ -3,6 +3,7 @@ export default function RootPage() {
     <html lang="zh">
       <head>
         <meta httpEquiv="refresh" content={`0; url=${process.env.homeUrl}`} />
+        <meta name="baidu-site-verification" content="codeva-l58Zj13Nvd" />
         <script
           dangerouslySetInnerHTML={{
             __html: `window.location.href = '${process.env.homeUrl}'`,
